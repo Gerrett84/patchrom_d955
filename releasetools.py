e7d3ce5197bdc7ec97c3db3f04b17ecb9dc5b4ab
Gerrett84 <gerrett84@gmail.com>
2014-06-25 18:11:02 +0200

    delete recursive /system

--- releasetools.py
+++ releasetools.py
@@ -5,20 +5,29 @@ def RemoveDeviceAssert(info):
   edify = info.script
   for i in xrange(len(edify.script)):
     if "ro.product" in edify.script[i]:
-      edify.script[i] = ''
+      edify.script[i] = '#'
       return
 
-def AddArgsForFormatSystem(info):
+def RemoveArgsForFormatSystem(info):
   edify = info.script
   for i in xrange(len(edify.script)):
     if "format(" in edify.script[i] and "msm_sdcc.1" in edify.script[i]:
-      edify.script[i] = ''
+      edify.script[i] = '#'
+      return
+
+def ReAddArgsForFormatSystem(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "package_extract_dir" in edify.script[i] and "system" in edify.script[i]:
+      edify.script[i] = 'delete_recursive("/system");package_extract_dir("system", "/system");'
       return
 
 def FullOTA_InstallEnd(info):
-    AddArgsForFormatSystem(info)
+    RemoveArgsForFormatSystem(info)
+    ReAddArgsForFormatSystem(info)
     RemoveDeviceAssert(info)
 
 def IncrementalOTA_InstallEnd(info):
-    AddArgsForFormatSystem(info)
+    RemoveArgsForFormatSystem(info)
+    ReAddArgsForFormatSystem(info)
     RemoveDeviceAssert(info)
