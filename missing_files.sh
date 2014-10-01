c6b31aa3bddf01b145182e933dca996e1bac479c
gerrett84 <gerrett84@gmail.com>
2014-10-01 23:56:24 +0200

    readd Metadata

--- missing_files.sh
+++ missing_files.sh
@@ -5,15 +5,15 @@ STOCKROM="$PORT_ROOT/d955/stockrom/"
 cd $STOCKROM
 
 echo "Download missing files..."
-wget -q http://roms.gerrett84.de/d955/miui/patchrom/jb42/missing_files2.zip
+wget -q http://roms.gerrett84.de/d955/miui/patchrom/kk/missing_files.zip
 
-if [ ! -s "missing_files2.zip" ];then
+if [ ! -s "missing_files.zip" ];then
     echo "Download failed, try again later"
     exit 8
 fi
 
 echo "unzipping.."
-unzip -q -o missing_files2.zip
+unzip -q -o missing_files.zip
 
 echo "remove zip"
 rm *.zip
